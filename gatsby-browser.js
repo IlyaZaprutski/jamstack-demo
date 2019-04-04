9411199fdd891d4757f886e89e27605554e4a423
Ilya Zaprutski <ilya.zaprutski@gmail.com>
2019-04-04 17:14:01 +0300

    add onServiceWorkerUpdateReady

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,11 @@
 // custom typefaces
 import 'typeface-montserrat';
 import 'typeface-merriweather';
+
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm('This application has been updated. ' + 'Reload to display the latest version?');
+
+    if (answer) {
+        window.location.reload();
+    }
+};
