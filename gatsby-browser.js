af93759d57ca4f98981233f1fc134a4e84d5007a
Ilya Zaprutski <ilya.zaprutski@gmail.com>
2019-04-05 16:59:33 +0300

    add browser apis

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,25 @@
 // custom typefaces
+import React from 'react';
+
 import 'typeface-montserrat';
 import 'typeface-merriweather';
+
+export const onClientEntry = () => {
+    console.log("We've started!");
+};
+
+export const onInitialClientRender = () => {
+    console.log('ReactDOM.render has executed');
+};
+
+export const onPrefetchPathname = ({ pathname }) => {
+    console.log(`Prefetch for ${pathname}`);
+};
+
+export const onPreRouteUpdate = ({ location, prevLocation }) => {
+    console.log('Gatsby started to change location to', location.pathname);
+    console.log('Gatsby started to change location from', prevLocation ? prevLocation.pathname : null);
+};
 
 export const onServiceWorkerUpdateReady = () => {
     const answer = window.confirm('This application has been updated. ' + 'Reload to display the latest version?');
@@ -9,3 +28,7 @@ export const onServiceWorkerUpdateReady = () => {
         window.location.reload();
     }
 };
+
+export const wrapPageElement = ({ element }) => <div className="page-layout">{element}</div>;
+
+export const wrapRootElement = ({ element }) => <div className="gatsby-browser-root">{element}</div>;
